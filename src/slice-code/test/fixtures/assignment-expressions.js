2afb12cd35f0fc344e60644b8aa76f5813c21b3b
Kent C. Dodds <kent@doddsfamily.us>
2016-09-26 21:00:18 -0600

    test(AssignmentExpression): add assignment-expression tests

---
+++ src/slice-code/test/fixtures/assignment-expressions.js
@@ -0,0 +1,21 @@
+export {plusEqual, minusEqual, divideEqual, multiplyEqual}
+
+function plusEqual(thingOne, thingTwo) {
+  thingOne += thingTwo
+  return thingOne
+}
+
+function minusEqual(thingOne, thingTwo) {
+  thingOne -= thingTwo
+  return thingOne
+}
+
+function divideEqual(thingOne, thingTwo) {
+  thingOne /= thingTwo
+  return thingOne
+}
+
+function multiplyEqual(thingOne, thingTwo) {
+  thingOne *= thingTwo
+  return thingOne
+}
