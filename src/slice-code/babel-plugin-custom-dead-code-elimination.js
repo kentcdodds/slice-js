2111045beebb06e7e522eb7024aca5459fc110c6
Kent C. Dodds <kent@doddsfamily.us>
2016-10-06 23:48:02 -0700

    feat(dead): remove unused variables

--- src/slice-code/babel-plugin-custom-dead-code-elimination.js
+++ src/slice-code/babel-plugin-custom-dead-code-elimination.js
@@ -10,13 +10,20 @@ function customDeadCodeElimination({types: t}) {
         }
       },
       VariableDeclarator(path) {
-        const {id} = path.node
+        const id = path.get('id')
         if (t.isObjectPattern(id)) {
-          id.properties.forEach(objectProperty => {
-            findAndRemoveUnusedBindings(path, objectProperty.value)
+          id.get('properties').forEach(objectProperty => {
+            findAndRemoveUnusedBindings(objectProperty, objectProperty.get('value'))
           })
-        } else if (t.isIdentifier) {
-          findAndRemoveUnusedBindings(path, id)
+        } else if (t.isArrayPattern(id)) {
+          id.get('elements').forEach(element => {
+            findAndRemoveUnusedBindings(element, element)
+          })
+        } else if (t.isIdentifier(id)) {
+          const keepReferences = true // TODO, there appears to be a bug with
+          findAndRemoveUnusedBindings(path, id, keepReferences)
+        } else {
+          throw new Error(`slice-js does not yet support VariableDeclarators with an id of type ${id.type}`)
         }
       },
       ExpressionStatement(path) {
@@ -25,18 +32,49 @@ function customDeadCodeElimination({types: t}) {
           path.remove()
         }
       },
+      Program: {
+        // these are things we can only reasonably try to remove after everything else has been removed.
+        exit(path) {
+          path.traverse({
+            ObjectPattern(objPath) {
+              if (!objPath.node.properties.length) {
+                // `var {} = foo` // don't ask me why....
+                objPath.parentPath.remove()
+              }
+            },
+          })
+        },
+      },
     },
   }
-}
 
-function findAndRemoveUnusedBindings(path, identifier) {
-  const binding = path.scope.getBinding(identifier.name)
-  if (!binding || !binding.referencePaths.length || isReferencingOnlyItself(identifier, binding)) {
-    path.remove()
+  function findAndRemoveUnusedBindings(path, identifier) {
+    if (isRemoved(path)) {
+      return
+    }
+    const referencePaths = getReferencePaths(path, identifier.node.name)
+    if (isReferencingNothingButItself(identifier.node, referencePaths)) {
+      path.remove()
+    }
   }
-}
 
-function isReferencingOnlyItself(identifier, binding) {
-  return binding.referencePaths.length === 1 &&
-    binding.referencePaths[0].node === identifier
+  function getReferencePaths(path, name) {
+    const binding = path.scope.getBinding(name) || {referencePaths: []}
+    return binding.referencePaths.filter(isNotRemoved)
+  }
+
+  function isReferencingNothingButItself(identifier, referencePaths) {
+    return !referencePaths.length ||
+      (referencePaths.length === 1 && referencePaths[0].node === identifier)
+  }
+
+  function isRemoved(path) {
+    // if the path has a parent that has no node, then it has been removed
+    return !!path.find(parent => !parent.node)
+  }
+
+  function isNotRemoved(path) {
+    return !isRemoved(path)
+  }
+
 }
